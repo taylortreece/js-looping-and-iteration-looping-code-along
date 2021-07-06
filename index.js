b144ac81a8c896a6a2a75a85ddc75f381c4d9aeb
taylortreece <tayloratreece@gmail.com>
2021-07-06 17:30:45 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Code your solutions in this file
+function writeCards(array, event) {
+    let thankYouArray = []
+    for (let i = 0; i < array.length; i++) {
+        thankYouArray.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
+    }
+    return thankYouArray
+}
